41150cf6ee2db0a4083149a66ab2295599a04c35
lillabofinken <pbkrogen65@gmail.com>
2025-10-03 19:54:18 +0200

    Project

---
+++ EGG_GameJam/Source/EGG_GameJam/EGG_GameJam.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EGG_GameJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EGG_GameJam, "EGG_GameJam" );
